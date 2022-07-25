92f556d14e247b5f0dfa5e8a895eb58ee0b3e839
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-07-25 21:31:23 +0600

    update

--- api.php
+++ api.php
@@ -5,7 +5,7 @@ $REQ=$_SERVER['REQUEST_METHOD'];
 
 switch ($REQ) {
     case 'GET':
-        echo '{"name": "get ... taijul"}';
+        getmethod();
         break;
         case 'POST':
             echo '{"name": "post ... taijul"}';
@@ -18,9 +18,27 @@ switch ($REQ) {
                    
                             
     default:
-        
+    echo '{"name":  "data cant found"}';
         break;
 }
+
+
+function getmethod(){
+    global $con;
+    $querry="SELECT  * FROM new ";
+    $result=mysqli_query($con,$querry);
+     if(mysqli_num_rows($result)>0){
+        $rows=array();
+        while($r=mysqli_fetch_assoc($result)){
+            $rows['result'][]=$r;
+        }
+        echo json_encode($rows);
+     }else{
+        echo '{"result" : "no data found"}';
+     }
+}
+
+
 
 
 ?>
