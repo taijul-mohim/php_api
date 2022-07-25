79631d13dc3b4f6d7dd83f96e809ca59bec97100
taijul-mohim <59666569+taijul-mohim@users.noreply.github.com>
2022-07-25 19:53:10 +0600

    db connection

--- api.php
+++ api.php
@@ -1,5 +1,5 @@
 <?php
-
+include("db.php");
 header("content-type: apllocation/json");
 $REQ=$_SERVER['REQUEST_METHOD'];
 
---
+++ db.php
@@ -0,0 +1,16 @@
+<?php
+$serv="localhost";
+$usr="root";
+$pass="";
+$db="my_api";
+$con=new mysqli($serv,$usr,$pass,$db);
+if($con==true){
+    
+}
+
+
+
+
+
+
+?>
